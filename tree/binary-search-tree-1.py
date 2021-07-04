4c23ac4ae0e295810e3a3cb88e460cbb6ffb2aed
sandipan898 <sandipan.das898@gmail.com>
2021-07-05 00:59:46 +0530

    BST

--- tree/binary-search-tree-1.py
+++ tree/binary-search-tree-1.py
@@ -11,19 +11,19 @@ class BST:
         self.node_count = 0
     
     def insert(self, data):
-        self.insert_node(self.root, data)
+        self.root = self.insert_node(self.root, data)
         
     def insert_node(self, root, data):
-        new_node = Node(data)
-        if self.root == None:
-            self.root = new_node
+        if root == None:
             self.node_count += 1    
-            return
-        elif data <= self.root.data:
-            self.root.left = self.insert_node(self.root.left, data)
-        elif data > self.root.data:
-            self.root.right = self.insert_node(self.root.left, data)
-        self.node_count += 1
+            return Node(data)
+        elif data <= root.data:
+            print("Inserting {} {} of {}".format(data, 'left', root.data))
+            root.left = self.insert_node(root.left, data)
+        elif data > root.data:
+            print("Inserting {} {} of {}".format(data, 'right', root.data))
+            root.right = self.insert_node(root.left, data)
+        return root
 
     def traverse_preorder(self, root):
         if root is not None:
@@ -44,29 +44,30 @@ class BST:
             print(root.data, end=' ')
 
 
-def insert(root, data):
-    new_node = Node(data)
-    if root == None:
-        root = new_node
-        print(root.data)
-        return root
-    if data <= root.data:
-        root.left = insert(root.left, data)
-    elif data > root.data:
-        root.right = insert(root.left, data)
-    return root
+# def insert(root, data):
+#     new_node = Node(data)
+#     if root == None:
+#         root = new_node
+#         print(root.data)
+#         return root
+#     if data <= root.data:
+#         root.left = insert(root.left, data)
+#     elif data > root.data:
+#         root.right = insert(root.left, data)
+#     return root
 
 
 if __name__ == '__main__':
     bst_tree = BST()
-    bst_tree.root = insert(bst_tree.root, 30)
-    insert(bst_tree.root, 10)
-    insert(bst_tree.root, 20)
-    insert(bst_tree.root, 50)
-    insert(bst_tree.root, 30)
-    insert(bst_tree.root, 40)
-    insert(bst_tree.root, 25)
-    insert(bst_tree.root, 5)
+    bst_tree.insert(30)
+    bst_tree.insert(10)
+    bst_tree.insert(20)
+    bst_tree.insert(50)
+    bst_tree.insert(30)
+    bst_tree.insert(40)
+    bst_tree.insert(25)
+    bst_tree.insert(5)
     print(bst_tree.root.data)
     bst_tree.traverse_preorder(bst_tree.root)
     print(bst_tree.node_count)
+    
