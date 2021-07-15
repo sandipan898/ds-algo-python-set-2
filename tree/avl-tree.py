3b5ccb783c2edc7dda733fc3745e4b6e6bcc6a97
sandipan898 <sandipan.das898@gmail.com>
2021-07-15 22:02:32 +0530

    AVLTree deletion

--- tree/avl-tree.py
+++ tree/avl-tree.py
@@ -1,3 +1,5 @@
+import sys 
+
 class Node:
     def __init__(self, data):
         self.left = self.right = None
@@ -5,20 +7,21 @@ class Node:
         self.height = 1
 
 
-class ALVTree:
+class AVLTree:
     def get_height(self, root):
         if root is None:
-            # print("0 height")
             return 0
-        # print(root.data, root.height)
         return root.height
     
     def get_balance_factor(self, root):
         if root is None:
-            # print("0 bf")
             return 0
-        # print(root.data, root.height)
         return (self.get_height(root.left) - self.get_height(root.right))
+    
+    def get_min_value_node(self, root):
+        if root is None or root.left is None:
+            return root
+        return self.get_min_value_node(root.left)
 
     def insert(self, root, data):
         if root is None:
@@ -30,11 +33,9 @@ class ALVTree:
         
         root.height = 1 + max(self.get_height(root.left), self.get_height(root.right))
         bf = self.get_balance_factor(root)
-        # print(root.data, root.height, bf)
         
         if bf > 1:
             # Left Left Case
-            print(bf, root.data)
             if data < root.left.data:
                 return self.rotate_right(root)
             
@@ -52,6 +53,57 @@ class ALVTree:
             # Right Right Case
             elif data > root.right.data:
                 return self.rotate_left(root)
+        return root
+
+    def delete(self, root, data):
+        if root is None:
+            return None
+        print(root.data)
+        if data > root.data:
+            root.right = self.delete(root.right, data)
+        elif data < root.data:
+            root.left = self.delete(root.left, data)
+        
+        else:
+            if root.left is None:
+                return root.right
+            elif root.right is None:
+                return root.left
+
+        print(root.data)
+
+        min_node = self.get_min_value_node(root.right)
+        root.data = min_node.data
+        root.right = self.delete(root.right, min_node.data)
+        print(root.data)
+
+        if root is None:
+            return None
+
+        root.height = 1 + max(self.get_height(root.left), self.get_height(root.right))
+        bf = self.get_balance_factor(root)
+        
+        print(bf, root.data)
+        if bf > 1:
+            left_bf = self.get_balance_factor(root.left)
+            # Left Left Case
+            if self.get_balance_factor(root.left) >= 0:
+                return self.rotate_right(root)
+            # Left Right Case
+            if self.get_balance_factor(root.left) < 0:
+                root.left = self.rotate_left(root.left)
+                return self.rotate_right(root)
+
+        if bf < -1:
+            right_bf = self.get_balance_factor(root.right)
+            # Right Left Case
+            if self.get_balance_factor(root.right) >= 0:
+                root.right = self.rotate_right(root.right)
+                return self.rotate_left(root)
+            # Right Right Case
+            if self.get_balance_factor(root.right) < 0:
+                return self.rotate_left(root)
+
         return root
 
     def rotate_left(self, root):
@@ -81,9 +133,22 @@ class ALVTree:
         self.traverse_preorder(root.left)
         self.traverse_preorder(root.right)
 
+    # Print the tree
+    def printHelper(self, currPtr, indent, last):
+        if currPtr != None:
+            sys.stdout.write(indent)
+            if last:
+                sys.stdout.write("R----")
+                indent += "     "
+            else:
+                sys.stdout.write("L----")
+                indent += "|    "
+            print(currPtr.data)
+            self.printHelper(currPtr.left, indent, False)
+            self.printHelper(currPtr.right, indent, True)
 
 if __name__ == '__main__':
-    myTree = ALVTree()
+    myTree = AVLTree()
     root = None
     root = myTree.insert(root, 10)
     root = myTree.insert(root, 20)
@@ -107,7 +172,25 @@ if __name__ == '__main__':
     """
     
     # Preorder Traversal
-    print("Preorder traversal of the",
-        "constructed AVL tree is")
+    print("Preorder traversal of the constructed AVL tree is")
     myTree.traverse_preorder(root)
     print()
+
+    myTree = AVLTree()
+    root = None
+    nums = [9, 5, 10, 0, 6, 11, -1, 1, 2]
+    
+    for num in nums:
+        root = myTree.insert(root, num)
+    
+    print("Preorder Traversal after insertion -")
+    myTree.traverse_preorder(root)
+    print()
+    # myTree.printHelper(root, "", True)
+    
+    root = myTree.delete(root, 10)
+    
+    print("Preorder Traversal after deletion -")
+    myTree.traverse_preorder(root)
+    print()
+    # myTree.printHelper(root, "", True)
